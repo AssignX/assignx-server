e865a4d3669a15bfe04e6b816ac7af0ed144651b
jhssong <jhssong02@gmail.com>
2025-09-24 23:17:23 +0900

    init

---
+++ src/main/java/com/assignx/AssignxServer/AssignxServerApplication.java
@@ -0,0 +1,13 @@
+package com.assignx.AssignxServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignxServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignx/AssignxServer/AssignxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignx.AssignxServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignxServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
