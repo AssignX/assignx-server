38e487179eaa7ae3001a1b69f973798943921af7
jhssong <jhssong02@gmail.com>
2025-09-26 00:19:51 +0900

    feat: Add swagger

---
+++ src/main/java/com/assignx/AssignxServer/global/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.assignx.AssignxServer.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
+                        .anyRequest().permitAll() // TODO Permit all other requests for now
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
