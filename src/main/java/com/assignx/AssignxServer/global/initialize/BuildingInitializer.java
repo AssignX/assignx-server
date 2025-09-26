711cf64b7872149156456b7021df0f1ea5d9b365
jhssong <jhssong02@gmail.com>
2025-09-26 12:25:02 +0900

    feat: Add BuildingInitializer

---
+++ src/main/java/com/assignx/AssignxServer/global/initialize/BuildingInitializer.java
@@ -0,0 +1,36 @@
+package com.assignx.AssignxServer.global.initialize;
+
+import com.assignx.AssignxServer.domain.building.dto.AdminBuildingCreateReqDTO;
+import com.assignx.AssignxServer.domain.building.entity.Building;
+import com.assignx.AssignxServer.domain.building.service.BuildingService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BuildingInitializer implements CommandLineRunner {
+    private final ObjectMapper objectMapper;
+    private final BuildingService buildingService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        InputStream inputStream = new ClassPathResource("initialize/buildings.json").getInputStream();
+        List<AdminBuildingCreateReqDTO> dtos = objectMapper.readValue(inputStream, new TypeReference<>() {
+        });
+
+        List<Building> buildingsToSave = new ArrayList<>();
+        for (AdminBuildingCreateReqDTO dto : dtos) {
+            buildingService.addBuilding(dto);
+        }
+
+    }
+}
