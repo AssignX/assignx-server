1cf0cff47fb7e6f912c3c572ea9ac619fd746403
jhssong <jhssong02@gmail.com>
2025-10-05 03:24:49 +0900

    chore: Add comment in MemberController

--- src/main/java/com/assignx/AssignxServer/domain/member/controller/MemberController.java
+++ src/main/java/com/assignx/AssignxServer/domain/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.assignx.AssignxServer.domain.member.controller;
 
 import com.assignx.AssignxServer.domain.member.dto.MemberResDTO;
 import com.assignx.AssignxServer.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/search/employee")
+    @Operation(summary = "직원 조회", description = "직원 정보를 조회합니다.")
     public ResponseEntity<List<MemberResDTO>> getAllEmployeesByNameOrDepartment(
             @RequestParam String name, Long departmentId) {
         List<MemberResDTO> res = memberService.getAllEmployeesByNameOrDepartment(name, departmentId);
@@ -27,6 +29,7 @@ public class MemberController {
     }
 
     @PostMapping("/search/professor")
+    @Operation(summary = "교수 조회", description = "교수 정보를 조회합니다.")
     public ResponseEntity<List<MemberResDTO>> getAllProfessorsByName(@RequestParam String name) {
         List<MemberResDTO> res = memberService.getAllProfessorsByName(name);
         return ResponseEntity.ok(res);
