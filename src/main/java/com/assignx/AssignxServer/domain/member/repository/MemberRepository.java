113c1714293771c242048cdb2688481cd183b781
jhssong <jhssong02@gmail.com>
2025-11-25 10:35:00 +0900

    fix: Fix searchMember condition

--- src/main/java/com/assignx/AssignxServer/domain/member/repository/MemberRepository.java
+++ src/main/java/com/assignx/AssignxServer/domain/member/repository/MemberRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findByRoleAndName(Role role, String name);
+    List<Member> findByRoleAndNameContainingIgnoreCase(Role role, String name);
 
     List<Member> findByRoleAndDepartment(Role role, Department department);
 
